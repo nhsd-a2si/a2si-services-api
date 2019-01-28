3693393f8e38e0323d7efa1535241d986ab27f4d
Mark Henwood <mcbhenwood@gmail.com>
2019-01-28 18:24:32 +0000

    Require an override to run in debug mode

--- services/services/settings.py
+++ services/services/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG_MODE', False)
 
 ALLOWED_HOSTS = []
 
