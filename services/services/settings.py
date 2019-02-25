f577b5a48a126de69e727f8cad6e27f9695d8453
Mark Henwood <mcbhenwood@gmail.com>
2019-02-25 10:05:11 +0000

    Derive static files' URL from environment

--- services/services/settings.py
+++ services/services/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.environ.get('DJANGO_STATIC_URL', '/static/')
 
 # Collect static files and place in S3 bucket
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
