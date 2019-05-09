3be5177d0a84122372e1e42ee3c1e8147ed97d9b
Mark Henwood <mcbhenwood@gmail.com>
2019-05-09 19:32:07 +0100

    Refactor restriction code into a new APIView base class

---
+++ services/api/api_views.py
@@ -0,0 +1,8 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+
+
+class ServicesAPIView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
--- services/api/views.py
+++ services/api/views.py
@@ -1,17 +1,12 @@
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
+
+from .api_views import ServicesAPIView
 
 
-class PlaceholderView(APIView):
+class PlaceholderView(ServicesAPIView):
     """
     Simple placeholder view to verify installation.
 
     """
-    # TODO Refactor into a base view class that requires this
-    authentication_classes = (TokenAuthentication,)
-    permission_classes = (IsAuthenticated,)
-
     def get(self, request):
         return Response(dict(message='This is the Placeholder API'))
