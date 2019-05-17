665bb1d43d2efb42aca9edf7978e6db3634c7221
Mark Henwood <mcbhenwood@gmail.com>
2019-05-17 11:18:15 +0100

    Assert that anon 'UI' access same error as anon 'API' access

--- services/api/tests_ui.py
+++ services/api/tests_ui.py
@@ -2,6 +2,18 @@ from django.test import TestCase
 
 
 class UiHeaderTestCase(TestCase):
+    def test_anonymous_get_fails(self):
+        response = self.client.get('/placeholder/',
+                                   HTTP_ACCEPT='text/html')
+        self.assertEqual(
+            401,
+            response.status_code
+        )
+        self.assertEqual(
+            response['WWW-Authenticate'],
+            'Token'
+        )
+
     def test_header_text_correct(self):
         response = self.client.get('/placeholder/',
                                    HTTP_ACCEPT='text/html')
